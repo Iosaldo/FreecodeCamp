629d224eb031ec6fa409294c84502c1453babeb6
Iosaldo <iyosaldonbk@gmail.com>
2025-05-10 18:54:30 +0000

    update

--- myApp.js
+++ myApp.js
@@ -1,7 +1,20 @@
 let express = require('express');
 let app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.use('/',(req, res, next) => {
+  res.send("Hello World");
+  console.log("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+}
+);
 
 
+const bodyParser = require('body-parser');
 
 
 
